d10a6b36dcaf8ecdc99249727a1c2682d2915c4e
Lizaveta Petrova <goldglaider@gmail.com>
2022-06-21 16:42:35 +0300

    docs: add TypeScript

--- Migration-to-TypeScript/src/components/controller/appLoader.js
+++ Migration-to-TypeScript/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '8a0c446e37a14e469e683932db32fed2', // получите свой ключ https://newsapi.org/
         });
     }
 }
