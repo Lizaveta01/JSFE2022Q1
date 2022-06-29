75bf395974f98840f84296dd9daadaf73e8a8c87
Lizaveta Petrova <goldglaider@gmail.com>
2022-06-29 21:01:14 +0300

    refactor: add comments to function in filter

--- Migration-to-TypeScript/src/components/view/sources/filter.ts
+++ Migration-to-TypeScript/src/components/view/sources/filter.ts
@@ -7,12 +7,11 @@ const filterCategory: string[] = ["general","business", "technology", "sports", 
 const filterCountry: string[] = ['us','au','no', 'it','sa', 'pk', 'gb', 'de', 'br', 'ca', 'es', 'ar', 'fr', 'in', 'is', 'ru', 'se', 'za', 'ie', 'nl', 'zh'];
 const filterLanguage: string[] = ['en', 'no', 'it', 'ar', 'ud', 'de', 'pt', 'es', 'fr', 'he', 'ru', 'sv', 'nl', 'zh'];
 
-
 createOption(filterCategory, sectionCategory);
 createOption(filterCountry, sectionCountry );
 createOption(filterLanguage, sectionLanguage);
 
-
+//load data to LocalStorage
 export function writeLocalStorage() {
     const categoryValue = sectionCategory.options[sectionCategory.selectedIndex].value;
     const countryValue = sectionCountry.options[sectionCountry.selectedIndex].value;
@@ -22,9 +21,8 @@ export function writeLocalStorage() {
     localStorage.setItem('language', languageValue);
   }
 
-
-
-  export function createOption(text:string[], select: HTMLSelectElement) {
+//make additional option to select
+export function createOption(text:string[], select: HTMLSelectElement) {
     text.forEach((item) => {
         const option: HTMLOptionElement = document.createElement('option');
         option.value = item;
@@ -33,13 +31,14 @@ export function writeLocalStorage() {
     }
      )}
 
-     document.addEventListener('DOMContentLoaded', function(){
-        const categoryValue = localStorage.getItem('category');
-        const countryValue = localStorage.getItem('country');
-        const languageValue = localStorage.getItem('language');
-        if(categoryValue && countryValue && languageValue) {
-            sectionCategory.value = categoryValue;
-            sectionCountry.value = countryValue;
-            sectionLanguage.value = languageValue;
-        }
-    });
+// help save information after click 'show' and reload page
+document.addEventListener('DOMContentLoaded', function(){
+    const categoryValue = localStorage.getItem('category');
+    const countryValue = localStorage.getItem('country');
+    const languageValue = localStorage.getItem('language');
+    if(categoryValue && countryValue && languageValue) {
+        sectionCategory.value = categoryValue;
+        sectionCountry.value = countryValue;
+        sectionLanguage.value = languageValue;
+    }
+});
