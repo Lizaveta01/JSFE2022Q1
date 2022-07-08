21cb1d7c85b2c102510d618723431aa90aff63d5
Lizaveta Petrova <goldglaider@gmail.com>
2022-07-08 15:23:54 +0300

    feat: add function to added products in basket

--- online-store/src/components/sorting.ts
+++ online-store/src/components/sorting.ts
@@ -1,6 +1,2 @@
-export function search(){
 
-  
-
-}
 
--- online-store/src/index.ts
+++ online-store/src/index.ts
@@ -2,7 +2,6 @@ import './style.scss';
 import { slider } from './components/slider';
 import { draw } from './components/createcard';
 import { cards } from './components/cards_info';
-import { create } from 'nouislider';
 
 
 slider();
@@ -10,57 +9,65 @@ draw(cards);
 
 
 
-
 //================================================================SEARCH
-  let formSearch = document.querySelector('form');
+  
   let inputSearch = document.querySelector('#input-search') as HTMLInputElement;
-  let cardName = document.querySelectorAll('#shoes__name');
+   const products = document.querySelector('.products') as HTMLElement;
   
-  
-
-    const noRes = document.createElement('div');
-    noRes.classList.add ('noresult')
-    noRes.innerHTML= `Sorry, we couldn't find the page you're looking for`;
-    const main = document.querySelector('main');
-    main?.appendChild(noRes);
-
-
   inputSearch.oninput = function () {
-    let text = inputSearch.value.trim()
+  let text = inputSearch.value.trim();
+ 
+  products.innerHTML = "";
+  const notify = document.querySelector('.notify') as HTMLElement;
+  const catalog = document.querySelector('.catalog') as HTMLElement;
 
-    const products = document.querySelector('.products') as HTMLElement;
-    products.innerHTML = "";
-    const catalog = document.querySelector('.catalog');
-
-    
-
-    if(text != ''){
-      cards.forEach((el) => {
-        if(el.name.toLowerCase().search(text.toLowerCase()) == -1){
-          catalog?.classList.add('hiden');
-          noRes.classList.remove('hiden');
-          
-        } else {
-          catalog?.classList.remove('hiden');
-          noRes.classList.add('hiden');
-          const shoesCard = document.createElement('div');
-          shoesCard.classList.add('shoes-card');
-          shoesCard.innerHTML = `
-          <img src="${el.image}" alt="img">
-          <p id="shoes__name">${el.name}</p>
-          <p id="shoes__category">${el.category}</p>
-          <p id="shoes__colors">${el.color.length} colors</p>
-          <p id="shoes__price">$${el.price}</p>
-          <div class="basket-status"></div>`;
-          products.appendChild(shoesCard);
-        }      
-      });
-    } else {
-      catalog?.classList.remove('hiden');
-      noRes.classList.add('hiden');
-    }
-
+  if(text != ''){
+    cards.forEach((el) => {
+      if(el.name.toLowerCase().search(text.toLowerCase()) == -1){
+        notify.classList.remove('hiden');
+        catalog.classList.add('catalog__hiden');
+      } else {
+        notify.classList.add('hiden');
+        catalog.classList.remove('catalog__hiden');
+        const shoesCard = document.createElement('div');
+        shoesCard.classList.add('shoes-card');
+        shoesCard.innerHTML = `
+        <img src="${el.image}" alt="img">
+        <p id="shoes__name">${el.name}</p>
+        <p id="shoes__category">${el.category}</p>
+        <p id="shoes__colors">${el.color.length} colors</p>
+        <p id="shoes__price">$${el.price}</p>
+        <div class="basket-status"></div>`;
+        products.appendChild(shoesCard);
+      }      
+    });
+  } else {
+    notify.classList.add('hiden');
+    catalog.classList.remove('catalog__hiden');
   }
-  
-//==========================================================
+}
 
+//=======================================================HIDE FILTERS 
+const buttonHide = document.querySelector('#hide-filter') as HTMLElement;
+const filter = document.querySelector('.filter') as HTMLElement;
+buttonHide.onclick = function (){
+filter.classList.toggle('hiden');
+}
+
+//======================================================ADD TO BASKET
+const counterBasket = document.querySelector('.counter-products') as HTMLElement;
+let counter = 0;
+const inBasket = document.querySelectorAll('.shoes-card');
+
+inBasket.forEach(item => item.addEventListener('click', function(){
+  const status = item.querySelector('.basket-status') as HTMLElement;
+  status.classList.toggle('active');
+  if(status.classList.contains('active')){
+    counter++
+  } else {
+    counter = counter -1;
+  }
+  counterBasket.innerHTML = counter.toString();
+  console.log(status);
+}));
+//=======================================================
