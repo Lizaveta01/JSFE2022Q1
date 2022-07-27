ebb7943fd805559939b949b7e097636c6ca8cfdc
Lizaveta Petrova <goldglaider@gmail.com>
2022-07-27 11:53:49 +0300

    init: start async-race task

---
+++ async-race/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HtmlwebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+const baseConfig = {
+  mode: 'development',
+  entry: {
+    app: './src/index.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[hash][ext]',
+  },
+  module: {
+    rules: [
+      {
+          test: /\.[ts]s$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+      },
+      {
+        test: /\.(?:\\ico|gif|png|svg|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(html)$/,
+        use: ['html-loader'],
+      }
+    ],  
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
